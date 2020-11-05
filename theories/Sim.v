cf852962286a3d8942f5949764bf48eb7a9167e2
alxest <youngju.song@sf.snu.ac.kr>
2020-11-05 14:55:28 +0900

    WIP -- will try minki's approach

--- theories/Sim.v
+++ theories/Sim.v
@@ -41,38 +41,46 @@ Definition single X (x: X): X -> Prop := fun x0 => x = x0.
 Definition option_to_list X (ox: option X): list X := match ox with | Some x => [x] | _ => [] end.
 Coercion option_to_list_coercion := option_to_list.
 
-Inductive Star L (P: L.(state) -> Prop) (st0: L.(state)): (list event) -> (L.(state)) -> Prop :=
+Definition PStep L (P: L.(state) -> Prop) (st0: L.(state)) (ev: option event) (st1: (L.(state))): Prop :=
+  (<<PROP: P st0>>) /\ (<<STEP: L.(step) st0 ev st1>>)
+.
+
+Inductive PStar L (P: L.(state) -> Prop) (st0: L.(state)): (list event) -> (L.(state)) -> Prop :=
 | star_refl
     (PROP: P st0)
     (* ev_sum *)
     (* (EV: ev_sum = []) *)
   :
-    Star L P st0 [] st0
+    PStar L P st0 [] st0
 | star_step
-    (PROP: P st0)
     ev evs st1 st2
-    (STEP: L.(step) st0 ev st1)
-    (STAR: Star L P st1 evs st2)
+    (STEP: PStep L P st0 ev st1)
+    (STAR: PStar L P st1 evs st2)
     (* ev_sum *)
     (* (EV: ev_sum = ev ++ evs) *)
   :
-    Star L P st0 (ev ++ evs) st2
+    PStar L P st0 (ev ++ evs) st2
 .
 
-Inductive Plus L (P: L.(state) -> Prop) (st0: L.(state)): (list event) -> (L.(state)) -> Prop :=
+Inductive PPlus L (P: L.(state) -> Prop) (st0: L.(state)): (list event) -> (L.(state)) -> Prop :=
 | plus_intro
-    (PROP: P st0)
     ev evs st1 st2
-    (STEP: L.(step) st0 ev st1)
-    (STAR: Star L P st1 evs st2)
+    (STEP: PStep L P st0 ev st1)
+    (STAR: PStar L P st1 evs st2)
   :
-    Plus L P st0 (ev ++ evs) st2
+    PPlus L P st0 (ev ++ evs) st2
 .
 
+Definition DStep L (st0: (L.(state))) (ev: option event) (st1: L.(state)) :=
+  PStep L (fun st => L.(state_sort) st = demonic) st0 ev st1.
+Definition AStep L (st0: (L.(state))) (ev: option event) (st1: L.(state)) :=
+  PStep L (fun st => L.(state_sort) st = angelic) st0 ev st1.
 Definition DPlus L (st0: (L.(state))) (evs: list event) (st1: L.(state)) :=
-  Plus L (fun st => L.(state_sort) st = demonic) st0 evs st1.
+  PPlus L (fun st => L.(state_sort) st = demonic) st0 evs st1.
 Definition APlus L (st0: (L.(state))) (evs: list event) (st1: L.(state)) :=
-  Plus L (fun st => L.(state_sort) st = angelic) st0 evs st1.
+  PPlus L (fun st => L.(state_sort) st = angelic) st0 evs st1.
+Hint Unfold DStep AStep.
+Hint Unfold DPlus APlus.
 
 Lemma spin_nofinal
       L st0
@@ -86,22 +94,22 @@ Qed.
 
 Lemma spin_noevent
       L st0 e st1
-      (STAR: Star L (fun st => _.(state_sort) st = angelic) st0 [e] st1)
+      (STAR: PStar L (fun st => _.(state_sort) st = angelic) st0 [e] st1)
       (SPIN: Beh.state_spin _ st0)
   :
     False
 .
 Proof.
   remember [e] as tmp in STAR. revert Heqtmp.
-  induction STAR; ii; ss. punfold SPIN. inv SPIN; ii; rewrite PROP in *; ss.
+  induction STAR; ii; ss. punfold SPIN. rr in STEP; des. inv SPIN; ii; rewrite PROP in *; ss.
   destruct ev, evs; ss; clarify.
-  - exploit STEP0; eauto. i; des; ss.
-  - exploit STEP0; eauto. i; des; ss. pclearbot. eapply IHSTAR; eauto.
+  - exploit STEP; eauto. i; des; ss.
+  - exploit STEP; eauto. i; des; ss. pclearbot. eapply IHSTAR; eauto.
 Qed.
 
 Lemma spin_astar
       L st0 st1
-      (STAR: Star L (fun st => _.(state_sort) st = angelic) st0 [] st1)
+      (STAR: PStar L (fun st => _.(state_sort) st = angelic) st0 [] st1)
       (SPIN: Beh.state_spin _ st0)
   :
     <<SPIN: Beh.state_spin _ st1>>
@@ -109,8 +117,8 @@ Lemma spin_astar
 Proof.
   remember [] as tmp in STAR. revert Heqtmp.
   revert SPIN. induction STAR; ii; ss.
-  { destruct ev, evs; ss. dup SPIN. punfold SPIN. inv SPIN; rewrite PROP in *; ss.
-    exploit STEP0; eauto. i; des. pclearbot. eapply IHSTAR; ss.}
+  { destruct ev, evs; ss. dup SPIN. rr in STEP; des. punfold SPIN. inv SPIN; rewrite PROP in *; ss.
+    exploit STEP; eauto. i; des. pclearbot. eapply IHSTAR; ss. }
 Qed.
 
 
@@ -123,19 +131,47 @@ Section SIM.
   Variable ord: idx -> idx -> Prop.
 
   Variant _sim sim (i0: idx) (st_src0: L0.(state)) (st_tgt0: L1.(state)): Prop :=
-  | sim_final
-      (FINTGT: _.(state_sort) st_tgt0 = final)
-      (FINSRC: _.(state_sort) st_src0 = final)
-    :
-      _sim sim i0 st_src0 st_tgt0
+  (* | sim_final *)
+  (*     (FINTGT: _.(state_sort) st_tgt0 = final) *)
+  (*     (FINSRC: _.(state_sort) st_src0 = final) *)
+  (*   : *)
+  (*     _sim sim i0 st_src0 st_tgt0 *)
+
+  (* | sim_demonic *)
+  (*     (DEM: _.(state_sort) st_tgt0 = demonic) *)
+  (*     (STEPSIM: forall *)
+  (*         ev st_tgt1 *)
+  (*         (STEPTGT: _.(step) st_tgt0 ev st_tgt1) *)
+  (*       , *)
+  (*         exists i1 st_src1, *)
+  (*           (<<PLUS: DPlus _ st_src0 ev st_src1>> *)
+  (*            \/ (<<TAU: st_src1 = st_src0>> /\ <<TAU: ev = None>> /\ <<ORD: ord i1 i0>>)) *)
+  (*           /\ *)
+  (*           (<<SIM: sim i1 st_src1 st_tgt1>>)) *)
+  (*   : *)
+  (*     _sim sim i0 st_src0 st_tgt0 *)
+  (* | sim_angelic *)
+  (*     (ANG: _.(state_sort) st_src0 = angelic) *)
+  (*     (STEPSIM: forall *)
+  (*         ev st_src1 *)
+  (*         (STEPSRC: _.(step) st_src0 ev st_src1) *)
+  (*       , *)
+  (*         exists i1 st_tgt1, *)
+  (*           (<<PLUS: APlus _ st_tgt0 ev st_tgt1>> *)
+  (*            \/ (<<TAU: st_tgt1 = st_tgt0>> /\ <<TAU: ev = None>> /\ <<ORD: ord i1 i0>>)) *)
+  (*           /\ *)
+  (*           (<<SIM: sim i1 st_src1 st_tgt1>>)) *)
+  (*   : *)
+  (*     _sim sim i0 st_src0 st_tgt0 *)
   | sim_demonic
       (DEM: _.(state_sort) st_tgt0 = demonic)
+      (FIN: _.(state_sort) st_src0 = final <-> _.(state_sort) st_tgt0 = final)
       (STEPSIM: forall
           ev st_tgt1
           (STEPTGT: _.(step) st_tgt0 ev st_tgt1)
         ,
           exists i1 st_src1,
-            (<<PLUS: DPlus _ st_src0 ev st_src1>>
+            (<<STEP: DStep _ st_src0 ev st_src1>>
              \/ (<<TAU: st_src1 = st_src0>> /\ <<TAU: ev = None>> /\ <<ORD: ord i1 i0>>))
             /\
             (<<SIM: sim i1 st_src1 st_tgt1>>))
@@ -143,17 +179,26 @@ Section SIM.
       _sim sim i0 st_src0 st_tgt0
   | sim_angelic
       (ANG: _.(state_sort) st_src0 = angelic)
+      (FIN: _.(state_sort) st_src0 = final <-> _.(state_sort) st_tgt0 = final)
       (STEPSIM: forall
           ev st_src1
           (STEPSRC: _.(step) st_src0 ev st_src1)
         ,
           exists i1 st_tgt1,
-            (<<PLUS: APlus _ st_tgt0 ev st_tgt1>>
+            (<<STEP: AStep _ st_tgt0 ev st_tgt1>>
              \/ (<<TAU: st_tgt1 = st_tgt0>> /\ <<TAU: ev = None>> /\ <<ORD: ord i1 i0>>))
             /\
             (<<SIM: sim i1 st_src1 st_tgt1>>))
     :
       _sim sim i0 st_src0 st_tgt0
+
+
+
+
+
+
+
+
   (* | sim_demonic_tgt *)
   (*     (DEM: L1.(state_sort) st_tgt0 = demonic) *)
   (*     (DEMTGT: forall *)
@@ -220,8 +265,8 @@ Section SIM.
   Proof.
     ii. inv IN; try (by econs; eauto).
 
+    - econs 1; et. i. exploit STEPSIM; et. i; des; esplits; et.
     - econs 2; et. i. exploit STEPSIM; et. i; des; esplits; et.
-    - econs 3; et. i. exploit STEPSIM; et. i; des; esplits; et.
 
     (* - econs 2; et. ii. exploit DEMTGT; et. i; des; et. *)
     (* - econs 3; et. des. esplits; eauto. *)
@@ -259,38 +304,54 @@ Section SIM.
     rename x into i0. rename H into IH.
 
     punfold SIM. inv SIM.
-    - eapply spin_nofinal in SPIN; ss.
+    (* - eapply spin_nofinal in SPIN; ss. *)
     - punfold SPIN. inv SPIN; rewrite DEM in *; ss. des. clarify.
       rename st1 into st_tgt1.
       exploit STEPSIM; eauto. pclearbot. i; des; rr in SIM; des; ss; cycle 1.
       { clarify. eapply IH; eauto. }
-      inv PLUS. destruct ev, evs; ss. rename st_src1 into st_src2. rename st1 into st_src1.
-      clear - CIH STAR PROP STEP TL SIM.
-
-      gstep. econs 2; eauto. esplits; eauto. clear PROP STEP st_src0.
-      rename st_src1 into st_src0. rename st_src2 into st_src1.
-
-      remember [] as tmp in STAR. revert Heqtmp.
-      induction STAR; ii.
-      { gbase. eapply CIH; eauto. }
-      { rename st0 into st_src1. rename st1 into st_src2. rename st2 into st_src3.
-        destruct ev, evs; ss. gstep. econs 2; eauto. esplits; eauto. }
+      rr in STEP; des.
+      gstep. econs 2; eauto. esplits; eauto. gbase. eapply CIH; eauto.
     - gstep. econs; eauto. ii. exploit STEPSIM; eauto. i; des; ss; clarify; rr in SIM; des; ss; cycle 1.
       { rename st1 into st_src1. esplits; eauto. exploit IH; eauto.
         ii. eapply gpaco1_mon_gen; eauto. { eapply Beh.state_spin_mon. } ii; ss.
       }
-      inv PLUS.
+      rr in STEP0; des.
       punfold SPIN. inv SPIN; ss; rewrite PROP in *; ss.
-      exploit STEP1; eauto. i; des. clarify. rr in TL; des; ss. clarify.
-      rename st_tgt1 into st_tgt2. rename st0 into st_tgt1. rename st1 into st_src1.
-
-      destruct ev; ss.
-      { exfalso. eapply spin_noevent; eauto. }
-      esplits; eauto.
-
+      exploit STEP0; eauto. i; des. clarify. rr in TL; des; ss. clarify.
+      rename st1 into st_src1. esplits; eauto.
       gbase. eapply CIH; eauto.
-      eapply spin_astar; eauto.
   Qed.
+
+  Lemma adequacy_final
+        (WF: well_founded ord)
+        i0 st_src0 st_tgt0
+        (SIM: sim i0 st_src0 st_tgt0)
+        (FIN: Beh.of_state L1 st_tgt0 Tr.done)
+    :
+      <<FIN: Beh.of_state L0 st_src0 Tr.done>>
+  .
+  Proof.
+    punfold SIM; inv SIM.
+    - punfold FIN.
+      generalize dependent st_src0.
+      generalize dependent i0.
+      dependent induction FIN using Beh.of_state_ind; ii; try rewrite DEM in *; ss;
+        rename st0 into st_tgt0.
+      rr in STEP. des. clarify. rename st1 into st_tgt1.
+      exploit STEPSIM; eauto. i; des.
+      + rr in STEP; des; ss. rr in SIM; des; ss. exploit IH; ss; eauto.
+      +
+      apply FIN0 in FINAL.
+
+      inv FIN; try rewrite DEM in *; ss.
+      rr in STEP. des. clarify. inv TL.
+      apply FIN0 in FINAL.
+  Qed.
+
+  (* Record semantics_wf (L: semantics): Prop := mk_swf { *)
+  (*   swf_final: forall st0 (FIN: L.(state_sort) st0 = final),  *)
+  (* } *)
+  (* . *)
 
   Lemma adequacy_aux
         (WF: well_founded ord)
@@ -307,50 +368,62 @@ Section SIM.
     revert_until i0. pattern i0. eapply well_founded_ind; eauto. clear i0. i.
     rename x into i0. rename H into IH. rename x2 into tr.
 
+    {
+      punfold PR. generalize dependent st_src0.
+      induction PR using Beh.of_state_ind; ii; ss;
+        rename st0 into st_tgt0.
+      - (** done **)
+        punfold SIM. inv SIM; try rewrite H in *; ss.
+        + (** fin-fin **)
+          gstep. econs; eauto.
+        + (** asim **)
+          gstep. econs 7; eauto. rr. ii. rename st1 into st_src1.
+          exploit STEPSIM; eauto. i; des.
+          * destruct ev; ss.
+            -- (** some event **) destruct e; ss. right. esplits; eauto.
+    }
+
     destruct (classic (tr = Tr.spin)); clarify.
-    { punfold PR. inv PR; ss.
+    { punfold PR.
+      move PR before IH. revert_until PR.
+      dependent induction PR using Beh.of_state_ind; ii; ss.
       - exploit adequacy_spin; eauto. i. gstep. econs; eauto.
-      -
+      - rr in STEP. des. clarify. gstep.
+        rename st0 into st_tgt0. rename st1 into st_tgt1.
+        specialize (IH0 (eq_refl _)).
+        inv TL; ii; ss.
+        econs 5; et. admit.
+      - admit.
     }
 
     dup SIM.
     punfold SIM. inv SIM.
     - gstep. punfold PR. inv PR; ss; try rewrite FINTGT in *; clarify.
-      + (** fin **) econs; ss; et.
-      + (** spin **) eapply spin_nofinal in SPIN; ss.
+      (** fin **) econs; ss; et.
 
     - punfold PR.
       inv PR; ss; try rewrite DEM in *; clarify.
-      + (** spin **)
-        {
-          exploit CIH. eauto. pfold. econs 2; eauto. intro R.
-          punfold SPIN. inv SPIN; try rewrite DEM in *; ss. des. clarify. pclearbot.
-          rename st1 into st_tgt1.
-          exploit STEPSIM; eauto. i; des; r in SIM; des; ss; cycle 1.
-          { clarify. eapply IH; eauto. }
-          inv PLUS. rename st_src1 into st_src2. rename st1 into st_src1. destruct ev, evs; ss.
-          gstep. econs 5; eauto. rr. esplits; eauto.
-          exploit CIH. eauto. pfold. econs 2; eauto. intro RR.
-          inv STAR.
-          - econs.
-        }
-        punfold SPIN. inv SPIN; try rewrite DEM in *; ss. des. clarify. pclearbot.
-        rename st1 into st_tgt1.
-        exploit STEPSIM; eauto. i; des; r in SIM; des; ss; cycle 1.
-        { clarify. eapply IH; eauto. }
-        inv PLUS. rename st_src1 into st_src2. rename st1 into st_src1. destruct ev, evs; ss.
-        gstep. econs 5; eauto. rr. esplits; eauto.
-        inv STAR.
-        { econs; eauto. }
       + (** nb **) gstep; econs; eauto.
       + (** dem tau **)
         rr in STEP. des. clarify.
-        exploit DEMTGT; et. i; des. r in SIM; des; ss. eapply IH; eauto.
+        exploit STEPSIM; et. i; des; subst; r in SIM; des; ss; et.
+        inv PLUS.
+        destruct ev, evs; ss.
+        inv STAR.
+        { gstep. econs 5; eauto. rr. esplits; eauto. econs; eauto.
       + (** dem sys **)
         rr in STEP. des. clarify. r in TL; des; ss.
-        exploit DEMTGT; et. i; des. r in SIM; des; ss.
-        rename st1 into st_tgt1.
+        exploit STEPSIM; et. i; des; subst; r in SIM; des; ss.
+        inv PLUS.
+        destruct ev0; ss; clarify.
+        *
+          gstep. econs 6; eauto. rr. esplits; eauto.
+          admit.
+        *
+          gstep. econs 5; eauto. rr. esplits; eauto.
+          admit.
 
+    -
         gstep.
         pfold.
         (*** st_tgt0 --ev--> st_tgt1 ---> evs
