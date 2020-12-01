f2cfb6419776b323f5561c5bc656e70ea9460eb7
alxest <youngju.song@sf.snu.ac.kr>
2020-12-01 14:25:31 +0900

    Add comm/assoc lemma

--- theories/ModSem.v
+++ theories/ModSem.v
@@ -3,6 +3,9 @@ Require Import ITreelib.
 Require Import Universe.
 Require Import Skeleton.
 Require Import PCM.
+Require Import STS Behavior.
+
+Generalizable Variables E R A B C X Y.
 
 Set Implicit Arguments.
 
@@ -146,6 +149,173 @@ Section MODSEM.
     initial_ld := app md0.(initial_ld) md0.(initial_ld);
   |}
   .
+
+
+
+  Section INTERP.
+
+  Variable ms: t.
+
+  Let itr0: callE ~> itree Es :=
+    fun _ ce =>
+      trigger PushFrame;;
+      rv <- (ms.(sem) ce);;
+      trigger PopFrame;;
+      Ret rv
+  .
+  Let itr1: itree (rE +' eventE) val := (mrec itr0) _ (Call "main" nil).
+
+
+
+  Definition r_state: Type := ((mname -> GRA) * list GRA).
+  Definition handle_rE `{eventE -< E}: rE ~> stateT r_state (itree E) :=
+    fun _ e '(mrs, frs) =>
+      match frs with
+      | hd :: tl =>
+        match e with
+        | Put mn mr fr =>
+          guarantee(URA.updatable (URA.add (mrs mn) hd) (URA.add mr fr));;
+          Ret (((update mrs mn mr), fr :: tl), tt)
+        | MGet mn => Ret ((mrs, frs), mrs mn)
+        | FGet => Ret ((mrs, frs), hd)
+        | Forge fr => Ret ((mrs, (URA.add hd fr) :: tl), tt)
+        | Discard fr =>
+          rest <- trigger (Choose _);;
+          guarantee(hd = URA.add fr rest);;
+          Ret ((mrs, rest :: tl), tt)
+        | CheckWf mn =>
+          assume(URA.wf (URA.add (mrs mn) hd));;
+          Ret ((mrs, frs), tt)
+        | PushFrame =>
+          Ret ((mrs, URA.unit :: frs), tt)
+        | PopFrame =>
+          Ret ((mrs, tl), tt)
+        end
+      | _ => triggerNB
+      end.
+  Definition interp_rE `{eventE -< E}: itree (rE +' E) ~> stateT r_state (itree E) :=
+    State.interp_state (case_ handle_rE State.pure_state).
+  Definition initial_r_state: r_state :=
+    (fun mn => match List.find (fun mnr => dec mn (fst mnr)) ms.(initial_ld) with
+               | Some r => snd r
+               | None => URA.unit
+               end, []).
+  Let itr2: itree (eventE) val := snd <$> (interp_rE itr1) initial_r_state.
+
+
+
+  Let state: Type := itree eventE val.
+
+  Definition state_sort (st0: state): sort :=
+    match (observe st0) with
+    | TauF _ => demonic
+    | RetF rv => final rv
+    | VisF (Choose X) k => demonic
+    | VisF (Take X) k => angelic
+    | VisF (Syscall fn args m0) k => vis
+    end
+  .
+
+  Inductive step: state -> option event -> state -> Prop :=
+  | step_tau
+      itr
+    :
+      step (Tau itr) None itr
+  | step_choose
+      X k (x: X)
+    :
+      step (Vis (subevent _ (Choose X)) k) None (k x)
+  | step_take
+      X k (x: X)
+    :
+      step (Vis (subevent _ (Take X)) k) None (k x)
+  | step_syscall
+      fn args m0 k ev m1 rv
+      (SYSCALL: syscall_sem fn args m0 = (ev, m1, rv))
+    :
+      step (Vis (subevent _ (Syscall fn args m0)) k) (Some ev) (k (m1, rv))
+  .
+
+  Program Definition interp: semantics := {|
+    STS.state := state;
+    STS.step := step;
+    STS.initial_state := itr2;
+    STS.state_sort := state_sort;
+  |}
+  .
+  Next Obligation. inv STEP; inv STEP0; ss. csc. rewrite SYSCALL in *. csc. Qed.
+  Next Obligation. inv STEP; ss. Qed.
+  Next Obligation. inv STEP; ss. Qed.
+
+  End INTERP.
+
+  (*** TODO: probably we can make ModSem.t as an RA too. (together with Sk.t) ***)
+  (*** However, I am not sure what would be the gain; and there might be universe problem. ***)
+
+  Let add_comm_aux
+      md0 md1 stl0 str0
+      (SIM: stl0 = str0)
+    :
+      <<COMM: Beh.of_state (interp (add md0 md1)) stl0 <1= Beh.of_state (interp (add md1 md0)) str0>>
+  .
+  Proof.
+    revert_until md1.
+    pcofix CIH. i. pfold.
+    clarify.
+    punfold PR. induction PR using Beh.of_state_ind; ss.
+    - econs 1; et.
+    - econs 2; et.
+      clear CIH. clear_tac. revert_until md1.
+      pcofix CIH. i. punfold H0. pfold.
+      inv H0.
+      + econs 1; eauto. ii. ss. exploit STEP; et. i; des. right. eapply CIH; et. pclearbot. ss.
+      + econs 2; eauto. des. esplits; eauto. right. eapply CIH; et. pclearbot. ss.
+    - econs 4; et. pclearbot. right. eapply CIH; et.
+    - econs 5; et. rr in STEP. des. rr. esplits; et.
+    - econs 6; et. ii. exploit STEP; et. i; des. clarify.
+  Qed.
+
+  Theorem add_comm
+          md0 md1
+          (WF: wf (add md0 md1))
+    :
+      <<COMM: Beh.of_program (interp (add md0 md1)) <1= Beh.of_program (interp (add md1 md0))>>
+  .
+  Proof.
+    ii. ss. r in PR. r. eapply add_comm_aux; et.
+    rp; et. clear PR. cbn. do 1 f_equal.
+    f_equal; cycle 1.
+    - unfold initial_r_state. f_equal. apply func_ext. intros fn. ss. des_ifs.
+      + admit "ez: wf".
+      + admit "ez: wf".
+      + admit "ez: wf".
+    - repeat f_equal. apply func_ext_dep. intro T. apply func_ext. intro c. destruct c.
+      repeat f_equal. apply func_ext. i. f_equal. des_ifs.
+      + admit "ez: wf".
+      + admit "ez: wf".
+  Qed.
+
+  Theorem add_assoc
+          md0 md1 md2
+          (WF: wf (add md0 (add md1 md2)))
+    :
+      <<COMM: Beh.of_program (interp (add md0 (add md1 md2))) <1=
+              Beh.of_program (interp (add (add md0 md1) md2))>>
+  .
+  Proof.
+    admit "TODO".
+  Qed.
+
+  Theorem add_assoc_rev
+          md0 md1 md2
+          (WF: wf (add md0 (add md1 md2)))
+    :
+      <<COMM: Beh.of_program (interp (add md0 (add md1 md2))) <1=
+              Beh.of_program (interp (add (add md0 md1) md2))>>
+  .
+  Proof.
+    admit "TODO".
+  Qed.
 
 End MODSEM.
 End ModSem.
