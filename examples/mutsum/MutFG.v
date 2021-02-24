6410e8722a5d4956041906500f4f65b844914baa
minkiminki <minki.cho@sf.snu.ac.kr>
2021-02-24 20:20:56 +0900

    Fix to compile

--- examples/mutsum/MutFG.v
+++ examples/mutsum/MutFG.v
@@ -18,33 +18,6 @@ Set Implicit Arguments.
 
 
 
-
-
-
-
-
-
-
-
-Hint Rewrite unfold_interp_mrec : itree_axiom2.
-Hint Rewrite bind_ret_l : itree_axiom2.
-Hint Rewrite bind_ret_r : itree_axiom2.
-Hint Rewrite bind_tau : itree_axiom2.
-Hint Rewrite bind_vis : itree_axiom2.
-(* Hint Rewrite bind_trigger : itree_axiom. *)
-Hint Rewrite bind_bind : itree_axiom2.
-Tactic Notation "irw" "in" ident(H) := repeat (autorewrite with itree_axiom2 in H; cbn in H).
-Tactic Notation "irw" := repeat (autorewrite with itree_axiom2; cbn).
-
-
-
-
-
-
-
-
-
-
 Section PROOF.
 
   Definition Σ: GRA.t := fun _ => URA.of_RA RA.empty.
@@ -54,14 +27,14 @@ Section PROOF.
   Infix "⋅" := URA.add (at level 50, left associativity).
   Notation "(⋅)" := URA.add (only parsing).
 
-  Definition FG0: Mod.t := add_list [MutMain0.main ; MutF0.F ; MutG0.G].
+  Definition FG0: Mod.t := Mod.add_list [MutMain0.main ; MutF0.F ; MutG0.G].
 
-  Definition FG1: Mod.t := add_list [MutMain1.main ; MutF1.F ; MutG1.G].
+  Definition FG1: Mod.t := Mod.add_list [MutMain1.main ; MutF1.F ; MutG1.G].
 
   Definition FG2: Mod.t := {|
     Mod.get_modsem := fun _ => {|
         ModSem.fnsems := List.map (fun '(fn, sb) => (fn, fun_to_src sb.(fsb_body))) (mainsbtb ++ (Fsbtb ++ Gsbtb));
-        ModSem.initial_mrs := [("Main", (ε, unit↑)) ; ("F", (ε, unit↑)) ; ("G", (ε, unit↑))];
+        ModSem.initial_mrs := [("Main", (ε, tt↑)) ; ("F", (ε, tt↑)) ; ("G", (ε, tt↑))];
       |};
     Mod.sk := Sk.unit;
   |}
@@ -70,7 +43,7 @@ Section PROOF.
   Definition FG3: Mod.t := {|
     Mod.get_modsem := fun _ => {|
         ModSem.fnsems := [("main", fun _ => Ret (Vint 55)↑) ; ("f", fun _ => triggerUB) ; ("g", fun _ => triggerUB)];
-        ModSem.initial_mrs := [("Main", (ε, unit↑)) ; ("F", (ε, unit↑)) ; ("G", (ε, unit↑))];
+        ModSem.initial_mrs := [("Main", (ε, tt↑)) ; ("F", (ε, tt↑)) ; ("G", (ε, tt↑))];
       |};
     Mod.sk := Sk.unit;
   |}
@@ -78,7 +51,7 @@ Section PROOF.
 
   Lemma FG01_correct: Beh.of_program (Mod.interp FG0) <1= Beh.of_program (Mod.interp FG1).
   Proof.
-    eapply sim_list_adequacy_closed. econs; [|econs; [|econs; ss]].
+    eapply ModPair.sim_list_adequacy_closed. econs; [|econs; [|econs; ss]].
     - split; auto. ii. ss. eapply MutMain01proof.correct.
     - split; auto. ii. ss. eapply MutF01proof.correct.
     - split; auto. ii. ss. eapply MutG01proof.correct.
@@ -95,21 +68,12 @@ Section PROOF.
 
   Lemma FG23_correct: Beh.of_program (Mod.interp FG2) <1= Beh.of_program (Mod.interp FG3).
   Proof.
-    eapply adequacy_local_closed. econs; auto. ii.
+    eapply ModPair.adequacy_local_closed. econs; auto. ii.
     eapply ModSemPair.mk with (wf:=top1) (le:=top2); ss.
     econs; [|econs; [|econs;ss]].
-    - econs; ss. ii. subst. exists 100.
-      unfold fun_to_src, cfun, body_to_src, mainBody, interp_hCallE_src, unwrapN, triggerNB.
-      ired. des_ifs.
-      + ired. repeat rewrite interp_trigger. ired.
-        pfold. econs; eauto. i. left.
-        pfold. ired. econs; eauto. left.
-        pfold. econs; eauto.
-      + ired. pfold. econs; eauto. ss.
-    - econs; ss. ii. subst. exists 100.
-      ss. pfold. econs; ss.
-    - econs; ss. ii. subst. exists 100.
-      ss. pfold. econs; ss.
+    - init. unfold fun_to_src, cfun, body_to_src, mainBody, interp_hCallE_src. steps.
+    - init. unfold fun_to_src, cfun, body_to_src, mainBody, interp_hCallE_src. steps.
+    - init. unfold fun_to_src, cfun, body_to_src, mainBody, interp_hCallE_src. steps.
   Qed.
 
   Theorem FG_correct: Beh.of_program (Mod.interp FG0) <1= Beh.of_program (Mod.interp FG3).
