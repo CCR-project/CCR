c4a311705c7dfcb4259df98889ec85145fb5b53a
alxest <youngju.song@sf.snu.ac.kr>
2021-02-25 14:40:51 +0900

    Fix broken file

--- theories/Hoare.v
+++ theories/Hoare.v
@@ -41,7 +41,7 @@ Section CANCEL.
 
   Hypothesis WTY: ms_tgt.(ModSem.fnsems) = List.map (fun '(fn, sb) => (fn, fun_to_tgt stb fn sb)) sbtb.
   Hypothesis WF1: Forall (fun '(_, sp) => In sp.(mn) (List.map fst ms_tgt.(ModSem.initial_mrs))) stb.
-  Hypothesis MAIN: List.find (fun '(_fn, _) => dec "main" _fn) stb = Some ("main", (@mk_simple _ "Main" unit (fun _ _ _ o => o = ord_top) top3)).
+  Hypothesis MAIN: List.find (fun '(_fn, _) => dec "main" _fn) stb = Some ("main", (@mk_simple _ "Main" unit (fun _ _ o _ => o = ord_top) top3)).
   Hypothesis WFR: URA.wf (rsum (ModSem.initial_r_state ms_tgt)).
 
   Theorem adequacy_type: Beh.of_program (Mod.interp md_tgt) <1= Beh.of_program (Mod.interp md_src).
