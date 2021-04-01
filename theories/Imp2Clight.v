b1144f41485eb26097a93ebc17e2a7a701047121
DJL <dongjae.lee@sf.snu.ac.kr>
2021-04-01 14:50:46 +0900

    catchup sts2itree branch

---
+++ theories/Imp2Clight.v
@@ -0,0 +1,15 @@
+From compcert Require Import Clight.
+From compcert Require Import Memory.
+From compcert Require Import Ctypes.
+
+Require Import Coqlib.
+Require Import Universe.
+Require Import Skeleton.
+Require Import PCM.
+Require Import STS Behavior.
+Require Import Any.
+Require Import ModSem.
+Require Import SimSTS.
+Require Import STSNorm.
+Require Import Imp.
+
