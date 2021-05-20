afcd89d0e30d63272818daafe75eaccd368b8732
ldj <dongjae.lee@sf.snu.ac.kr>
2021-05-20 14:17:06 +0900

    WIP: sim, assign

--- imp/Imp2ClightSim.v
+++ imp/Imp2ClightSim.v
@@ -42,7 +42,7 @@ Section SIM.
 
   Local Open Scope smallstep_scope.
 
-  Variant _sim sim (i0: idx) (st_src0: L0.(STS.state)) (st_tgt0: L1.(Smallstep.state)): Prop :=
+  Variant _sim sim (i0 j0: idx) (st_src0: L0.(STS.state)) (st_tgt0: L1.(Smallstep.state)): Prop :=
   | sim_fin
       retv
       (RANGE: (0 <= retv <= Int.max_unsigned)%Z)
@@ -52,7 +52,43 @@ Section SIM.
          forall (s : Smallstep.state L1) (r1 r2 : int),
            Smallstep.final_state L1 s r1 -> Smallstep.final_state L1 s r2 -> r1 = r2)
     :
-      _sim sim i0 st_src0 st_tgt0
+      _sim sim i0 j0 st_src0 st_tgt0
+
+  | sim_demonic_src
+      (SRT: _.(state_sort) st_src0 = demonic)
+      (SIM: exists st_src1
+          (STEP: _.(step) st_src0 None st_src1)
+        ,
+          exists i1, <<ORD: ord i1 i0>> /\ <<SIM: exists j1, sim i1 j1 st_src1 st_tgt0>>)
+    :
+      _sim sim i0 j0 st_src0 st_tgt0
+  | sim_demonic_tgt_dtm
+      (*** WRONG DEF, Note: UB in tgt ***)
+      (* (SIM: forall st_tgt1 *)
+      (*     (STEP: Step L1 st_tgt0 E0 st_tgt1) *)
+      (*   , *)
+      (*     exists i1, <<ORD: ord i1 i0>> /\ <<SIM: sim i1 st_src0 st_tgt1>>) *)
+      (DTM: strict_determinate_at L1 st_tgt0)
+      (SIM: exists st_tgt1
+          (STEP: Step L1 st_tgt0 E0 st_tgt1)
+        ,
+          exists j1, <<ORD: ord j1 j0>> /\ <<SIM: exists i1, sim i1 j1 st_src0 st_tgt1>>)
+      (*** equivalent def ***)
+      (* st_tgt1 *)
+      (* (STEP: Step L1 st_tgt0 E0 st_tgt1) *)
+      (* i1 *)
+      (* (ORD: ord i1 i0) *)
+      (* (SIM: sim i1 st_src0 st_tgt1) *)
+    :
+      _sim sim i0 j0 st_src0 st_tgt0
+  | sim_angelic_src
+      (SRT: _.(state_sort) st_src0 = angelic)
+      (SIM: forall st_src1
+          (STEP: _.(step) st_src0 None st_src1)
+        ,
+          exists i1, <<ORD: ord i1 i0>> /\ <<SIM: exists j1, sim i1 j1 st_src1 st_tgt0>>)
+    :
+      _sim sim i0 j0 st_src0 st_tgt0
 
   (* | sim_vis *)
   (*     (SRT: _.(state_sort) st_src0 = vis) *)
@@ -65,58 +101,21 @@ Section SIM.
   (*   : *)
   (*     _sim sim i0 st_src0 st_tgt0 *)
 
-  | sim_demonic_src
-      (SRT: _.(state_sort) st_src0 = demonic)
-      (SIM: exists st_src1
-          (STEP: _.(step) st_src0 None st_src1)
-        ,
-          exists i1, <<ORD: ord i1 i0>> /\ <<SIM: sim i1 st_src1 st_tgt0>>)
-    :
-      _sim sim i0 st_src0 st_tgt0
-  | sim_demonic_tgt_dtm
-      (*** WRONG DEF, Note: UB in tgt ***)
-      (* (SIM: forall st_tgt1 *)
-      (*     (STEP: Step L1 st_tgt0 E0 st_tgt1) *)
-      (*   , *)
-      (*     exists i1, <<ORD: ord i1 i0>> /\ <<SIM: sim i1 st_src0 st_tgt1>>) *)
-      (DTM: strict_determinate_at L1 st_tgt0)
-      (SIM: exists st_tgt1
-          (STEP: Step L1 st_tgt0 E0 st_tgt1)
-        ,
-          exists i1, <<ORD: ord i1 i0>> /\ <<SIM: sim i1 st_src0 st_tgt1>>)
-      (*** equivalent def ***)
-      (* st_tgt1 *)
-      (* (STEP: Step L1 st_tgt0 E0 st_tgt1) *)
-      (* i1 *)
-      (* (ORD: ord i1 i0) *)
-      (* (SIM: sim i1 st_src0 st_tgt1) *)
-    :
-      _sim sim i0 st_src0 st_tgt0
-  | sim_angelic_src
-      (SRT: _.(state_sort) st_src0 = angelic)
-      (SIM: forall st_src1
-          (STEP: _.(step) st_src0 None st_src1)
-        ,
-          exists i1, <<ORD: ord i1 i0>> /\ <<SIM: sim i1 st_src1 st_tgt0>>)
-    :
-      _sim sim i0 st_src0 st_tgt0
-
-
-  | sim_demonic_both
-      (SRT: _.(state_sort) st_src0 = demonic)
-      (DTM: strict_determinate_at L1 st_tgt0)
-      (SIM: exists st_tgt1
-          (STEP: Step L1 st_tgt0 E0 st_tgt1)
-        ,
-          exists st_src1 (STEP: _.(step) st_src0 None st_src1),
-            <<SIM: exists i1, sim i1 st_src1 st_tgt1>>)
-    :
-      _sim sim i0 st_src0 st_tgt0
+  (* | sim_demonic_both *)
+  (*     (SRT: _.(state_sort) st_src0 = demonic) *)
+  (*     (DTM: strict_determinate_at L1 st_tgt0) *)
+  (*     (SIM: exists st_tgt1 *)
+  (*         (STEP: Step L1 st_tgt0 E0 st_tgt1) *)
+  (*       , *)
+  (*         exists st_src1 (STEP: _.(step) st_src0 None st_src1), *)
+  (*           <<SIM: exists i1, sim i1 st_src1 st_tgt1>>) *)
+  (*   : *)
+  (*     _sim sim i0 st_src0 st_tgt0 *)
   .
 
-  Definition sim: _ -> _ -> _ -> Prop := paco3 _sim bot3.
+  Definition sim: _ -> _ -> _ -> _ -> Prop := paco4 _sim bot4.
 
-  Lemma sim_mon: monotone3 _sim.
+  Lemma sim_mon: monotone4 _sim.
   Proof.
     ii. inv IN.
 
@@ -124,7 +123,6 @@ Section SIM.
     - econs 2; et. des. esplits; et.
     - econs 3; et. des. esplits; et.
     - econs 4; et. i. exploit SIM; et. i; des. esplits; et.
-    - econs 5; et. des. esplits; et.
   Qed.
 
   Hint Constructors _sim.
@@ -134,7 +132,7 @@ Section SIM.
   Record simulation: Prop := mk_simulation {
     sim_wf_ord: <<WF: well_founded ord>>;
     sim_init: forall st_tgt0 (INITT: L1.(Smallstep.initial_state) st_tgt0),
-        exists i0, (<<SIM: sim i0 L0.(initial_state) st_tgt0>>);
+        exists i0 j0, (<<SIM: sim i0 j0 L0.(initial_state) st_tgt0>>);
     (* sim_init: exists i0 st_tgt0, (<<SIM: sim i0 L0.(initial_state) st_tgt0>>) /\ *)
     (*                              (<<INITT: L1.(Smallstep.initial_state) st_tgt0>>); *)
     (* sim_dtm: True; *)
@@ -149,6 +147,8 @@ End SIM.
 
 Section PROOF.
 
+  Import ModSemL.
+
   Context `{Σ: GRA.t}.
 
   Variable mmem : Mem.t -> Memory.Mem.mem -> Prop.
@@ -160,19 +160,33 @@ Section PROOF.
   (* Let src_sem := ModL.compile (Mod.add_list ([src_mod] ++ [Mem])). *)
   (* Let tgt_sem := semantics2 tgt. *)
 
+  Ltac sim_red := Red.prw ltac:(_red_gen) 2 0.
+  Ltac sim_tau i j := sim_red; econs 2; ss; clarify; eexists; exists (step_tau _); exists i; split; auto; exists j.
+
   Theorem match_states_sim
           (src: Imp.program) tgt ist cst
-          (COMP: compile src = OK tgt)
+          (COMP: Imp2Clight.compile src = OK tgt)
           (MS: match_states mmem ist cst)
     :
-      <<SIM: sim (ModL.compile (Mod.add_list ([Mem] ++ [ImpMod.get_mod src]))) (semantics2 tgt) lt 100 ist cst>>.
+      <<SIM: sim (ModL.compile (Mod.add_list ([Mem] ++ [ImpMod.get_mod src]))) (semantics2 tgt) lt 100 100 ist cst>>.
   Proof.
     move COMP before tgt.
     revert_until COMP.
     pcofix CIH. i. pfold.
     inv MS. destruct code.
     - ss. unfold itree_of_cont_stmt, itree_of_imp_cont, itree_of_imp_pop.
-      rewrite interp_imp_Skip. clarify. grind.
+      rewrite interp_imp_Assign. destruct e.
+      + rewrite interp_imp_expr_Var. grind. sim_red.
+        destruct (alist_find v le) eqn:AFIND; ss.
+        * sim_tau 99 101. left. pfold. sim_tau 98 102. left. pfold.
+          sim_red. destruct (classic (wf_val v0)).
+          { unfold assume. grind. econs 4; ss; clarify.
+            i. dependent destruction STEP; ss; clarify.
+            unfold trigger in STEP. ss. inv STEP; clarify. 
+          
+
+
+      clarify. grind.
       Red.prw ltac:(_red_gen) 2 0.
       inv MCS; clarify.
       + 
