53473eb52f631e751686cc8ad322c3a20844083e
alxest <youngju.song@sf.snu.ac.kr>
2021-06-07 20:57:20 +0900

    Housekeeping

--- examples/new_stack/NewStack1.v
+++ examples/new_stack/NewStack1.v
@@ -15,32 +15,14 @@ Set Implicit Arguments.
 
 
 
-(*** TODO: replace the one in AList.v ***)
-Global Instance function_Map `{Dec K} V: (Map K V (K -> option V)) :=
-  Build_Map
-    (fun _ => None)
-    (fun k0 v m => fun k1 => if dec k0 k1 then Some v else m k1)
-    (fun k0 m => fun k1 => if dec k0 k1 then None else m k1)
-    (fun k m => m k)
-    (fun m0 m1 => fun k => match (m0 k) with
-                           | Some v => Some v
-                           | _ => m1 k
-                           end).
-
 Section PROOF.
 
   Context `{Σ: GRA.t}.
   Context `{@GRA.inG memRA Σ}.
 
-  (*** TODO: move to OpenDef; align it with trivial_fspec2 ***)
-  Definition trivial_bottom_spec: ftspec unit unit :=
-    (mk_ksimple (fun (_: unit) => ((fun _ _ => (⌜False⌝: iProp)%I),
-                                   (fun _ => (⌜True⌝: iProp)%I))))
-  .
-
-  Definition new_spec: ftspec unit unit := trivial_bottom_spec.
-  Definition pop_spec: ftspec unit unit := trivial_bottom_spec.
-  Definition push_spec: ftspec unit unit := trivial_bottom_spec.
+  Definition new_spec: ftspec unit unit := kspec_trivial_bottom.
+  Definition pop_spec: ftspec unit unit := kspec_trivial_bottom.
+  Definition push_spec: ftspec unit unit := kspec_trivial_bottom.
 
   Notation pget := (p0 <- trigger PGet;; `p0: (gmap mblock (list Z)) <- p0↓ǃ;; Ret p0) (only parsing).
   Notation pput p0 := (trigger (PPut (p0: (gmap mblock (list Z)))↑)) (only parsing).
