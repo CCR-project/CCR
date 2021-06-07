39903482c149c293a953c67c26db62f2223737b3
minkiminki <minki.cho@sf.snu.ac.kr>
2021-06-07 19:18:04 +0900

    Remove admits in IPM and SimGlobal

--- lib/DisableSsreflect.v
+++ lib/DisableSsreflect.v
@@ -56,49 +56,40 @@ Ltac mf_equal := f_equal.
 Ltac f_equal := mf_equal.
 
 Module REWRITETEST.
-  Definition x: nat.
-  Proof.
-    exact 100.
-  Qed.
+  Section TEST.
+    Variable x y: nat.
+    Hypothesis XY: x = y.
 
-  Definition y: nat.
-  Proof.
-    exact 100.
-  Qed.
-
-  Lemma XY: x = y.
-  Proof.
-  Admitted.
-
-  Goal forall (a b c: nat) (EQ0: a = b) (EQ1: a = c) (EQ2: c = b) (EQ3: x = y),
-      a + b + y = c + c + x.
-  Proof.
-    (* intros. *)
-    (* rewrite EQ0 in *. Undo. *)
-    (* rewrite EQ1 in EQ0. Undo. *)
-    (* rewrite EQ0. Undo. *)
-    (* rewrite <- EQ0 in *. Undo. *)
-    (* rewrite <- EQ2 in EQ0. Undo. *)
-    (* rewrite <- EQ2. Undo. *)
-    (* rewrite ! EQ0 in *. Undo. *)
-    (* rewrite ! EQ1 in EQ0. Undo. *)
-    (* rewrite ! EQ0. Undo. *)
-    (* rewrite <- ! EQ0 in *. Undo. *)
-    (* rewrite <- ! EQ2 in EQ0. Undo. *)
-    (* rewrite <- ! EQ2. Undo. *)
-    (* rewrite XY in *. Undo. *)
-    (* rewrite XY in EQ3. Undo. *)
-    (* rewrite XY. Undo. *)
-    (* rewrite <- XY in *. Undo. *)
-    (* rewrite <- XY in EQ3. Undo. *)
-    (* rewrite <- XY. Undo. *)
-    (* rewrite ! XY in *. Undo. *)
-    (* rewrite ! XY in EQ3. Undo. *)
-    (* rewrite ! XY. Undo. *)
-    (* rewrite <- ! XY in *. Undo. *)
-    (* rewrite <- ! XY in EQ3. Undo. *)
-    (* rewrite <- ! XY. Undo. *)
-  Abort.
+    Goal forall (a b c: nat) (EQ0: a = b) (EQ1: a = c) (EQ2: c = b) (EQ3: x = y),
+        a + b + y = c + c + x.
+    Proof.
+      intros.
+      rewrite EQ0 in *. Undo.
+      rewrite EQ1 in EQ0. Undo.
+      rewrite EQ0. Undo.
+      rewrite <- EQ0 in *. Undo.
+      rewrite <- EQ2 in EQ0. Undo.
+      rewrite <- EQ2. Undo.
+      rewrite ! EQ0 in *. Undo.
+      rewrite ! EQ1 in EQ0. Undo.
+      rewrite ! EQ0. Undo.
+      rewrite <- ! EQ0 in *. Undo.
+      rewrite <- ! EQ2 in EQ0. Undo.
+      rewrite <- ! EQ2. Undo.
+      rewrite XY in *. Undo.
+      rewrite XY in EQ3. Undo.
+      rewrite XY. Undo.
+      rewrite <- XY in *. Undo.
+      rewrite <- XY in EQ3. Undo.
+      rewrite <- XY. Undo.
+      rewrite ! XY in *. Undo.
+      rewrite ! XY in EQ3. Undo.
+      rewrite ! XY. Undo.
+      rewrite <- ! XY in *. Undo.
+      rewrite <- ! XY in EQ3. Undo.
+      rewrite <- ! XY. Undo.
+    Abort.
+  End TEST.
 End REWRITETEST.
 
 Require Import Basics.
