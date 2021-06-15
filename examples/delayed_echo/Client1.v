ef23a2644ee5448b672f4b701ecb9142582ecc26
minkiminki <minki.cho@sf.snu.ac.kr>
2021-06-15 09:44:40 +0900

    Fix delayed echo client proofs

--- examples/delayed_echo/Client1.v
+++ examples/delayed_echo/Client1.v
@@ -19,16 +19,16 @@ Section PROOF.
 
   Context `{Σ: GRA.t}.
 
-  Let getint_spec: fspec := (mk_simple (X:=unit) (fun _ => ((fun _ o => (⌜o = ord_top⌝: iProp)%I), top2))).
-  Let putint_spec: fspec := (mk_simple (X:=unit) (fun _ => ((fun _ o => (⌜o = ord_top⌝: iProp)%I), top2))).
+  Let getint_spec: fspec := (mk_simple (X:=unit) (fun _ => ((fun _ o => (⌜o = ord_top⌝: iProp)%I), fun _ => (True: iProp)%I))).
+  Let putint_spec: fspec := (mk_simple (X:=unit) (fun _ => ((fun _ o => (⌜o = ord_top⌝: iProp)%I), fun _ => (True: iProp)%I))).
 
   Definition ClientStb: list (gname * fspec).
     eapply (Seal.sealing "stb").
     apply [("getint", getint_spec) ; ("putint", putint_spec)].
   Defined.
 
-  Definition getint_body: list val -> itree (hCallE +' pE +' eventE) val := resum_ktr getintF.
-  Definition putint_body: list val -> itree (hCallE +' pE +' eventE) val := resum_ktr putintF.
+  Definition getint_body: list val -> itree (hCallE +' pE +' eventE) val := getintF.
+  Definition putint_body: list val -> itree (hCallE +' pE +' eventE) val := putintF.
 
   Definition ClientSbtb: list (gname * fspecbody) :=
     [("getint", mk_specbody getint_spec (cfun getint_body)); ("putint", mk_specbody putint_spec (cfun putint_body))]
