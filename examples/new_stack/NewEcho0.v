7345f8062b94dfc30e36dc689a723ec9fdcfea5a
alxest <youngju.song@sf.snu.ac.kr>
2021-06-19 00:21:05 +0900

    Add NewEcho0

---
+++ examples/new_stack/NewEcho0.v
@@ -0,0 +1,99 @@
+Require Import Coqlib.
+Require Import Universe.
+Require Import STS.
+Require Import Behavior.
+Require Import ModSem.
+Require Import Skeleton.
+Require Import PCM.
+Require Import Mem1.
+Require Import TODOYJ.
+
+Set Implicit Arguments.
+
+
+
+(*** TODO: move to Universe ***)
+Global Program Instance val_dec: Dec val.
+Next Obligation.
+  repeat (decide equality).
+Defined.
+
+
+
+Section PROOF.
+
+  Context `{Σ: GRA.t}.
+
+  Definition is_minus_one (v: val): bool :=
+    match v with
+    | Vint i => dec i (- 1)%Z
+    | _ => false
+    end
+  .
+  (* void echo() { *)
+  (*   void* h = Stack.new(); *)
+  (*   input(h); *)
+  (*   output(h); *)
+  (* } *)
+
+  Definition echo_body: list val -> itree Es val :=
+    fun args =>
+      _ <- (pargs [] args)?;;
+      `h: val    <- (ccall "new" ([]: list val));;
+      `_: val    <- (ccall "input" ([h]: list val));;
+      `_: val    <- (ccall "output" ([h]: list val));;
+      Ret Vundef
+  .
+
+  (* void echo_get(void* h) { *)
+  (*   int n = IO.getint(); *)
+  (*   if(n == INT_MIN) return; *)
+  (*   Stack.push(h, n); *)
+  (*   echo_get(h); *)
+  (* } *)
+  
+  Definition input_body: list val -> itree Es val :=
+    fun args =>
+      h <- (pargs [Tuntyped] args)?;;
+      `n: val    <- (ccall "getint" ([]: list val));;
+      if (dec n (Vint min_64))
+      then Ret Vundef
+      else
+        `_: val    <- (ccall "push" ([h; n]: list val));;
+        `_: val    <- (ccall "input" ([h]: list val));;
+        Ret Vundef
+  .
+
+  (* void echo_put(void* h) { *)
+  (*   int n = Stack.pop(h); *)
+  (*   if(n == INT_MIN) return; *)
+  (*   IO.putint(n); *)
+  (*   echo_put(h); *)
+  (* } *)
+
+  Definition output_body: list val -> itree Es val :=
+    fun args =>
+      h <- (pargs [Tuntyped] args)?;;
+      `n: val    <- (ccall "pop" ([h]: list val));;
+      if (dec n (Vint min_64))
+      then Ret Vundef
+      else
+        `_: val    <- (ccall "putint" ([n]: list val));;
+        `_: val    <- (ccall "output" ([h]: list val));;
+        Ret Vundef
+  .
+
+  Definition EchoSem: ModSem.t := {|
+    ModSem.fnsems := [("echo", cfun echo_body); ("input", cfun input_body); ("output", cfun output_body)];
+    ModSem.mn := "Echo";
+    ModSem.initial_mr := ε;
+    ModSem.initial_st := tt↑;
+  |}
+  .
+
+  Definition Echo: Mod.t := {|
+    Mod.get_modsem := fun _ => EchoSem;
+    Mod.sk := Sk.unit;
+  |}
+  .
+End PROOF.
