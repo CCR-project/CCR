3bfa2fe6777eab6c56d64e12fa5b0c167cdf5e90
ldj <dongjae.lee@sf.snu.ac.kr>
2021-06-19 22:03:27 +0900

    WIP: right arrow

--- imp/Imp2CsharpminorSepComp.v
+++ imp/Imp2CsharpminorSepComp.v
@@ -264,19 +264,94 @@ Section PROOFRIGHT.
 
   (* Proving the right arrow of the diagram *)
 
+  Lemma pre_comp_link_two
+        src1 src2 pcs1 pcs2
+        (PCS1: pre_compile_iFuns (List.map snd (prog_funsL src1)) = Some pcs1)
+        (PCS2: pre_compile_iFuns (List.map snd (prog_funsL src2)) = Some pcs2)
+    :
+      <<PCS12: pre_compile_iFuns (List.map snd (l_prog_funsLM src1 src2)) = Some (pcs1 ++ pcs2)>>.
+  Proof.
+    unfold l_prog_funsLM. rewrite map_app. red.
+    match goal with
+    | [ |- pre_compile_iFuns (?_fs1 ++ ?_fs2) = _ ] => set (fs1:=_fs1) in *; set (fs2:=_fs2) in *
+    end.
+    unfold pre_compile_iFuns in *. des_ifs; ss; clarify.
+    { repeat f_equal. rewrite map_app. rewrite map_app. ss. }
+    exfalso. apply n. clear n. rewrite map_app. apply Forall_app; eauto.
+  Qed.
+
   Lemma link_then_some_gmap
-        src1 gm1 src2 gm2 srcl
+        src1 pcs1 gm1 src2 pcs2 gm2 srcl
+        (PCS1: pre_compile_iFuns (List.map snd (prog_funsL src1)) = Some pcs1)
         (GMAP1 : get_gmap src1 = Some gm1)
+        (PCS2: pre_compile_iFuns (List.map snd (prog_funsL src2)) = Some pcs2)
         (GMAP2 : get_gmap src2 = Some gm2)
         (LINK : link_imp src1 src2 = Some srcl)
     :
-      <<GMAPL: exists gml, get_gmap srcl = Some gml>>.
+      <<GMAPL: get_gmap srcl =
+               Some (mk_gmap (compile_eVars (l_ext_vars src1 src2))
+                             (compile_eFuns (l_ext_funs src1 src2))
+                             (compile_iVars (l_prog_varsL src1 src2))
+                             (pcs1 ++ pcs2))>>.
   Proof.
-    unfold link_imp in LINK. des_ifs.
+    unfold link_imp in LINK. des_ifs. unfold get_gmap; ss. erewrite pre_comp_link_two; eauto.
+    uo; ss. unfold get_gmap in *. des_ifs; ss. uo; des_ifs; ss; clarify. exfalso. apply n; clear n.
+    rename l into NOREPET1, l0 into NOREPET2, Heq2 into PCS1, Heq1 into PCS2.
+    bsimpl; des. rename Heq into LC1, Heq1 into LC2, Heq0 into LC3. unfold link_imp_cond2 in LC2. bsimpl; des.
+    repeat match goal with | [ H: _ = true |- _ ] => apply sumbool_to_bool_true in H end.
+  Admitted.
 
+  Lemma ext_vars_names :
+    forall src, <<EVN: List.map fst (compile_eVars (ext_varsL src)) = List.map s2p (ext_varsL src)>>.
+  Proof.
+    i. unfold compile_eVars. rewrite List.map_map. apply List.map_ext. i. ss.
+  Qed.
 
+  Lemma ext_funs_names :
+    forall src, <<EFN: List.map fst (compile_eFuns (ext_funsL src)) = List.map (compose s2p fst) (ext_funsL src)>>.
+  Proof.
+    i. unfold compile_eFuns. rewrite List.map_map. apply List.map_ext. i. destruct a. ss.
+  Qed.
 
+  Lemma int_vars_names :
+    forall src, <<IVN: List.map fst (compile_iVars (prog_varsL src)) = List.map (compose s2p fst) (prog_varsL src)>>.
+  Proof.
+    i. unfold compile_iVars. rewrite List.map_map. apply List.map_ext. i. destruct a; ss.
+  Qed.
 
+  Lemma int_funs_names :
+    forall src pcs
+      (PCS : pre_compile_iFuns (List.map snd (prog_funsL src)) = Some pcs),
+      <<IFN: List.map fst pcs = List.map (compose s2p (compose fst snd)) (prog_funsL src)>>.
+  Proof.
+    i. unfold pre_compile_iFuns in PCS. des_ifs. rewrite List.map_map in f. do 3 rewrite List.map_map. red.
+    apply map_ext_strong. i. apply List.Forall_map in f. rewrite Forall_forall in f. apply f in IN.
+    des_ifs. ss. destruct x. ss. destruct p. clarify.
+  Qed.
+
+  src1 : programL
+  pcs1 : list (ident * globdef fundef2 ())
+  src2 : programL
+  pcs2 : list (ident * globdef fundef2 ())
+  PCS1 : pre_compile_iFuns (List.map snd (prog_funsL src1)) = Some pcs1
+  PCS2 : pre_compile_iFuns (List.map snd (prog_funsL src2)) = Some pcs2
+  LC1 : Coqlib.list_norepet (List.map fst (l_prog_varsL src1 src2) ++ List.map fst (List.map snd (l_prog_funsLM src1 src2)))
+  LC2 : Coqlib.list_norepet (ext_varsL src1 ++ List.map fst (List.map snd (l_prog_funsLM src1 src2)))
+  LC5 : Coqlib.list_norepet (ext_varsL src2 ++ List.map fst (List.map snd (l_prog_funsLM src1 src2)))
+  LC4 : Coqlib.list_norepet (List.map fst (ext_funsL src1) ++ List.map fst (l_prog_varsL src1 src2))
+  LC0 : Coqlib.list_norepet (List.map fst (ext_funsL src2) ++ List.map fst (l_prog_varsL src1 src2))
+  LC3 : link_imp_cond3 src1 src2 = true
+  NOREPET1 : Coqlib.list_norepet
+               (List.map fst
+                  (pcs2 ++ compile_eFuns (ext_funsL src2) ++ compile_iVars (prog_varsL src2) ++ compile_eVars (ext_varsL src2)))
+  NOREPET2 : Coqlib.list_norepet
+               (List.map fst
+                  (pcs1 ++ compile_eFuns (ext_funsL src1) ++ compile_iVars (prog_varsL src1) ++ compile_eVars (ext_varsL src1)))
+  ============================
+  Coqlib.list_norepet
+    (List.map fst
+       ((pcs1 ++ pcs2) ++
+        compile_eFuns (l_ext_funs src1 src2) ++ compile_iVars (l_prog_varsL src1 src2) ++ compile_eVars (l_ext_vars src1 src2)))
 
 
 
