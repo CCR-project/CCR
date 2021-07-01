c796322341e3c200794b9cb55b99b46a1228176e
ldj <dongjae.lee@sf.snu.ac.kr>
2021-07-01 17:59:45 +0900

    new echo imps

---
+++ examples/new_echo/NewEchoMainImp.v
@@ -0,0 +1,39 @@
+Require Import Universe.
+Require Import PCM.
+Require Import ModSem.
+Require Import Imp.
+Require Import ImpNotations.
+
+Set Implicit Arguments.
+
+
+
+Section ECHO.
+
+  Context `{Σ: GRA.t}.
+
+  Import ImpNotations.
+  Local Open Scope expr_scope.
+  Local Open Scope stmt_scope.
+
+  Definition main :=
+    mk_function
+      []
+      []
+      (
+        @ "echoImp" []
+      ).
+
+  Definition EchoMain_prog : program :=
+    mk_program
+      "EchoMainImp"
+      []
+      [("echoImp", 0)]
+      []
+      [("echoMainImp", main)]
+  .
+
+  Definition EchoMainSem ge : ModSem.t := ImpMod.modsem EchoMain_prog ge.
+  Definition EchoMain : Mod.t := ImpMod.get_mod EchoMain_prog.
+
+End ECHO.
