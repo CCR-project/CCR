63a0eb20c91ae95fda5a246b52565f68cf455698
alxest <youngju.song@sf.snu.ac.kr>
2021-07-09 16:03:39 +0900

    Fix run.sh for os X

--- extract/run.sh
+++ extract/run.sh
@@ -1,3 +1,7 @@
-sed -i 's/let sort : Coq__Order.t list -> Coq__Order.t list/let sort/' AList.ml &&
-sed -i 's/let sort/let sort : Coq__Order.t list -> Coq__Order.t list/' AList.ml &&
+#!/bin/bash
+perl -i -pe's/let sort : Coq__Order.t list -> Coq__Order.t list/let sort/g' AList.ml &&
+  perl -i -pe's/let sort/let sort : Coq__Order.t list -> Coq__Order.t list/g' AList.ml &&
+  #sed works in linux but not OS X
+#sed -i 's/let sort : Coq__Order.t list -> Coq__Order.t list/let sort/' AList.ml &&
+#sed -i 's/let sort/let sort : Coq__Order.t list -> Coq__Order.t list/' AList.ml &&
 ocamlbuild main.native && ./main.native
