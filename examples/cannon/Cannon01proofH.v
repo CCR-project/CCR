f58ba289594b062e49332c8687076455a2f8a8a6
Minki Cho <minki.cho@sf.snu.ac.kr>
2022-01-13 14:39:31 +0900

    Comments

--- examples/cannon/Cannon01proofH.v
+++ examples/cannon/Cannon01proofH.v
@@ -12,30 +12,40 @@ Set Implicit Arguments.
 
 
 Section SIMMODSEM.
-
   Context `{Σ: GRA.t}.
   Context `{@GRA.inG CannonRA Σ}.
 
-  Let W: Type := Any.t * Any.t.
-
   Variable GlobalStb: Sk.t -> gname -> option fspec.
 
-  Let wf: unit -> Any.t -> Any.t -> iProp :=
-    (fun (_: unit) st_src st_tgt => ((⌜st_src = (1: Z)↑ /\ st_tgt = (1: Z)↑⌝ ** OwnM (Ready))
-                                     ∨ OwnM (Fired))%I)
-  .
+  (* Kripke world *)
+  Let W: Type := unit.
 
-  Let top2_PreOrder: @PreOrder unit top2.
-  Proof. econs; eauto. Qed.
-  Local Existing Instance top2_PreOrder.
+  (* world future relation *)
+  Let le: W -> W -> Prop := top2.
+
+  Let le_PreOrder: @PreOrder unit le.
+  Proof. unfold le. econs; eauto. Qed.
+  Local Existing Instance le_PreOrder.
+
+  (* state invariant *)
+  Let wf: W -> Any.t -> Any.t -> iProp :=
+        fun _ st_src st_tgt => ((⌜st_src = (1: Z)↑ /\ st_tgt = (1: Z)↑⌝ ** OwnM (Ready))
+                                ∨ OwnM (Fired))%I
+  .
 
   Theorem correct: refines2 [Cannon0.Cannon] [Cannon1.Cannon GlobalStb].
   Proof.
-    eapply adequacy_local2. econs; ss.
-    i. econstructor 1 with (le:=top2) (wf:=mk_wf wf); et; ss; cycle 1.
+    (* proof using local simulation *)
+    eapply adequacy_local2. econs; ss. i. red.
+    econstructor 1 with (le:=le) (wf:=mk_wf wf); et; ss; cycle 1.
+
+    (* initial state *)
     { exists tt. econs. eapply to_semantic.
       iIntros "H". iLeft. iSplitR; ss. }
-    econs; ss. econs; ss.
+
+    (* function "fire" *)
+    econs; ss. econs; ss. red.
+    (* use IPM *)
     apply isim_fun_to_tgt; auto. i; ss.
     unfold Cannon0.fire_body, Cannon1.fire_body.
     iIntros "[INV PRE]".
