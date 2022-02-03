ada0733363abbee2718cfe7392325c2de72d1ea0
damhiya <damhiya@gmail.com>
2022-02-03 14:49:23 +0900

    open proof mode

---
+++ examples/heapsort/Heapsort12proof.v
@@ -0,0 +1,66 @@
+Require Import Coqlib.
+Require Import ImpPrelude.
+Require Import STS.
+Require Import Behavior.
+Require Import ModSem.
+Require Import Skeleton.
+Require Import PCM.
+Require Import HoareDef.
+Require Import ProofMode.
+Require Import STB.
+Require Import HeapsortHeader.
+Require Import Heapsort1 Heapsort2.
+Require Import HTactics ProofMode.
+Require Import SimModSem.
+
+Set Implicit Arguments.
+
+Section SIMMODSEM.
+
+  Context `{Σ : GRA.t}.
+
+  Variable GlobalStb : Sk.t -> gname -> option fspec.
+
+  Definition wf : _ -> Any.t * Any.t -> Prop :=
+    @mk_wf
+      _
+      unit
+      (fun _ st_src st_tgt => True)%I.
+
+  Lemma sim_create (sk : alist string Sk.gdef) :
+    sim_fnsem wf top2
+   ("create",
+   fun_to_tgt "Heapsort" (GlobalStb sk) {| fsb_fspec := create_spec; fsb_body := cfunN create_body |})
+   ("create", cfunU Heapsort1.create_body).
+  Admitted.
+
+  Lemma sim_heapify (sk : alist string Sk.gdef) :
+    sim_fnsem wf top2
+              ("heapify",
+               fun_to_tgt "Heapsort" (GlobalStb sk) {| fsb_fspec := heapify_spec; fsb_body := cfunN heapify_body |})
+              ("heapify", cfunU Heapsort1.heapify_body).
+  Admitted.
+
+  Lemma sim_heapsort (sk : alist string Sk.gdef) :
+    sim_fnsem wf top2
+              ("heapsort", fun_to_tgt "Heapsort" (GlobalStb sk) {| fsb_fspec := heapsort_spec; fsb_body := cfunN heapsort_body |})
+              ("heapsort", cfunU Heapsort1.heapsort_body).
+  Admitted.
+
+  Theorem correct : refines2 [Heapsort1.Heapsort] [Heapsort2.Heapsort GlobalStb].
+  Proof.
+    Search "adequacy_local".
+    eapply SimModSem.adequacy_local2; econs; ss.
+    i.
+    econstructor 1 with (wf := wf) (le := top2); et; ss; cycle 1.
+    { exists tt. red. econs. eapply to_semantic. iIntros. ss. }
+    econs; cycle 1.
+    econs; cycle 1.
+    econs; cycle 1.
+    econs.
+    - apply sim_heapsort.
+    - apply sim_heapify.
+    - apply sim_create.
+  Qed.
+
+End SIMMODSEM.
